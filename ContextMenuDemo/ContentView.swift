2f92ceff024a8e020b31db91275f9af8b35ab5a8
Shigasuresh@123 <shigasuresh546@gmail.com>
2020-03-02 22:18:29 +0530

    Initial Commit

---
+++ ContextMenuDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ContextMenuDemo
+//
+//  Created by shiga on 02/03/20.
+//  Copyright © 2020 shiga. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
