bd98bf7fe28eaa13a70c27b18210322a33c563e0
Shigasuresh@123 <shigasuresh546@gmail.com>
2020-03-02 22:45:26 +0530

    initial

--- ContextMenuDemo/ContentView.swift
+++ ContextMenuDemo/ContentView.swift
@@ -10,7 +10,59 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        
+        NavigationView {
+            
+            List {
+                
+                ForEach(0 ..< 50) { _ in
+                    HStack {
+        
+                        Image("steve")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .cornerRadius(25)
+                            .padding(5.0)
+                        
+                        VStack(alignment: .leading) {
+                            
+                        Text("Steve Jobs")
+                            .font(.headline)
+                            
+                            Text("9999999999")
+                                .font(.subheadline)
+                            
+                        }
+                    }
+                    .contextMenu {
+                        
+                        VStack {
+                            
+                        Button(action: {
+                        }) {
+                            Text("Message")
+                            Image(systemName: "message.fill")
+                        }
+                        
+                            Button (action: {
+                                
+                            }) {
+                                Text("Video")
+                                Image(systemName: "video.fill")
+                            }
+                            
+                            Button(action: {
+                                
+                            }) {
+                                Text("Call")
+                                Image(systemName: "phone.fill")
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle("Contacts")
+        }
     }
 }
 
